5c7bcee8c15128d96fa6a40848cd3df752e82bbd
Karla Araújo <karlachs.a@gmail.com>
2023-01-22 16:38:36 -0300

    feat(person-api): implement and handle ResourceNotFoundException

--- person-api/src/main/java/br/com/karla/personapi/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ person-api/src/main/java/br/com/karla/personapi/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.karla.personapi.exceptions.handler;
 
 import br.com.karla.personapi.exceptions.ExceptionResponse;
+import br.com.karla.personapi.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -25,6 +26,16 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 request.getDescription(false)
         );
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions (Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 
 }
