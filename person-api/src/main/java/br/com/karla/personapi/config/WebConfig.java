b6a8b0694f4488dc409f59df3ca59ad226570861
Karla Araújo <karlachs.a@gmail.com>
2023-06-26 09:59:36 -0300

    feat(person-api/content): add support to XML format

---
+++ person-api/src/main/java/br/com/karla/personapi/config/WebConfig.java
@@ -0,0 +1,27 @@
+package br.com.karla.personapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+        /*
+        *
+        * Configura a URL para aceitar um header param no formato: Accept: application/xml
+        *
+        */
+
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+    }
+}
--- person-api/src/main/java/br/com/karla/personapi/controllers/PersonController.java
+++ person-api/src/main/java/br/com/karla/personapi/controllers/PersonController.java
@@ -18,7 +18,7 @@ public class PersonController {
 
     @GetMapping(
             value = "/{id}",
-            produces = MediaType.APPLICATION_JSON_VALUE
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
     ) // Swagger exige que 'produces' seja mantido para documentar efetivamente
     public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception {
 
@@ -27,23 +27,23 @@ public class PersonController {
 
     @GetMapping(
             value = "/all",
-            produces = MediaType.APPLICATION_JSON_VALUE
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
     )
     public List<PersonVO> findAll() throws Exception {
         return service.findAll();
     }
 
     @PostMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
     )
     public PersonVO create(@RequestBody PersonVO person) throws Exception {
         return service.create(person);
     }
 
     @PostMapping(value = "/v2",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
     )
     public br.com.karla.personapi.data.vo.v2.PersonVO create(
             @RequestBody br.com.karla.personapi.data.vo.v2.PersonVO person
@@ -53,8 +53,8 @@ public class PersonController {
 
 
     @PutMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
     )
     public PersonVO update(@RequestBody PersonVO person) throws Exception {
         return service.update(person);
